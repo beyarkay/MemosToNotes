d6312ef6324e269ab67eee8205c4e2ac284b24bd
beyarkay <boydrkane@gmail.com>
2019-03-27 21:20:25 +0200

    Got pdf text extraction working with PyPDF2 Started reading all the text in the file

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import textract
 import os
 import PyPDF2
 
-PATH = "file.pdf"
-text = textract.process(PATH)
+ABSOLUTE_PATH = "/Users/boydkane/PycharmProjects/MemosToNotes"
+FILE_NAME_WITH_EXTENSION = "file.pdf"
+FILE_PATH = os.path.join(ABSOLUTE_PATH, FILE_NAME_WITH_EXTENSION)
 
-path = '.'
-
-files = os.listdir(path)
-for name in files:
-    print(name)
-
-print(text)
+pdf = open(FILE_PATH, 'rb')
+reader = PyPDF2.PdfFileReader(pdf)
+print(reader.getDocumentInfo())
+for i in range(reader.numPages):
+    page = reader.getPage(i)
+    print(page.extractText())
