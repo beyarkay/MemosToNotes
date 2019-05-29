4a7bf2d600aea7376102277864406d7a08140086
beyarkay <boydrkane@gmail.com>
2019-05-29 16:49:09 +0200

    Streamlining and pyplot modifications

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import glob
+import json
 import os
 
 import numpy as np
 import pytesseract
 from PIL import Image as PILImage
 from matplotlib import pyplot as plt
-from nltk.corpus import stopwords
 from wand.color import Color
 from wand.image import Image as WandImage
 
@@ -62,7 +62,7 @@ def get_text_from_pdf(pdf_path: str, root: str = "corpus", verbose: bool = True,
     return pdf_text
 
 
-def process_pdfs(root, verbose=True, reuse=True):
+def pdfs_to_texts(root, verbose=True, reuse=True):
     """
     Collects all the text from various memos into one dictionary, ready for analysis
     :param root:
@@ -72,7 +72,6 @@ def process_pdfs(root, verbose=True, reuse=True):
     """
     directory_with_pdfs = os.path.join(root, "pdfs")
     pdf_paths = sorted(glob.glob(os.path.join(directory_with_pdfs, "*.pdf")))
-    pdf_dict = {"all_text": ""}
 
     for i, pdf_path in enumerate(pdf_paths, 1):
         file_id = get_filename(pdf_path)
@@ -82,90 +81,83 @@ def process_pdfs(root, verbose=True, reuse=True):
             # The user doesn't want to reuse the already-processed files, or those files don't exist
             if verbose:
                 print("Processing '{}' ({} out of {})".format(pdf_path, i, len(pdf_paths)))
-            pdf_dict[pdf_path] = get_text_from_pdf(pdf_path, verbose=verbose, reuse=reuse)
 
             # Write the data to the appropriate text file
             with open(text_file_path, "w+") as text_file:
-                text_file.writelines(pdf_dict[pdf_path])
+                text_file.writelines(get_text_from_pdf(pdf_path, verbose=verbose, reuse=reuse))
         else:
             # already-processed files exist, and the user wants to use them
             if verbose:
-                print("'{}' Already exists. Using existing data. ({} out of {})".format(pdf_path, i, len(pdf_paths)))
-            with open(text_file_path, "r") as text_file:
-                pdf_dict[pdf_path] = text_file.readlines()
-
-        pdf_dict["all_text"] += "\n".join(pdf_dict[pdf_path])
-
-    return pdf_dict
+                print("'{}' Already exists. ({} out of {})".format(pdf_path, i, len(pdf_paths)))
 
 
-def get_words_and_freqs(dictionary, use_tokens_file=True):
+def text_to_json(root, extra_words=None):
     """
     From a given dictionary of pdf_name:text pairs, extract the word:frequency pairs into
     adjacent lists
-    :param dictionary:
-    :param use_tokens_file: WIP
+    :param root: Root file in which to work. Should contain root/txts and root/summaries
+    :param extra_words:
     :return: (words, frequencies) tuple
     """
-    if use_tokens_file:
-        # Only get results of the tokens in tokens.txt
-        with open("tokens.txt", "r") as tokens_file:
-            tokens = [token.strip() for token in tokens_file.readlines()]
+    txt_files = sorted(glob.glob(os.path.join(root, "txts", "*.txt")))
+    for txt_file in txt_files:
+        with open(txt_file, "r") as txt:
+            text = "\n".join(txt.readlines())
 
-        text = dictionary["all_text"]
-        frequencies = [text.count(word) for word in tokens]
-    else:
-        # Get frequencies of every single word
-        all_words: list = dictionary["all_text"].split()
-        unique_words = list(set([word.lower() for word in all_words]))
-        clean_words = []
-        for word in unique_words:
-            clean_word = word.replace(",", ", ")
-            if clean_word not in stopwords.words("english"):
-                clean_words.append(clean_word)
-        frequencies = [all_words.count(word) for word in clean_words]
+        # Preprocessing
+        # Remove all duplicates, and make all the words lowercase
+        unique_words = list(set([word.lower() for word in text.split()] + extra_words))
+        # Get the frequency of each word
+        frequencies = [text.count(word) for word in unique_words]
 
-    together = sorted(zip(tokens, frequencies), key=lambda x: x[1], reverse=True)
+        # sort the words by their frequency, descending
+        sorted_words = sorted(zip(unique_words, frequencies), key=lambda x: x[1], reverse=True)
+        clean_words = [item[0] for item in sorted_words]
+        frequencies = [item[1] for item in sorted_words]
 
-    clean_words = [item[0] for item in together]
-    frequencies = [item[1] for item in together]
-
-    with open("frequencies.txt", "w+") as file:
-        for word, freq in zip(clean_words, frequencies):
-            print("{0:<30}:{1}".format(word, freq), file=file)
-    return clean_words, frequencies
+        # write as json file
+        json_path = os.path.join(root, "summaries", get_filename(txt_file) + ".json")
+        json_dict = {word: freq for word, freq in zip(clean_words, frequencies)}
+        with open(json_path, "w+") as json_file:
+            json.dump(json_dict, json_file, indent=2)
 
 
-def plot_bar_graph(unique_words, frequencies):
+def json_to_graph(json_path, limit=100):
     """
     Plot and show a bar graph with the frequencies of the words in dictionary
     Case insensitive
+    :param json_path:
+    :param limit:
     :param unique_words:
     :param frequencies:
     """
 
-    index = np.arange(len(unique_words[:70]))
+    # json_path = "corpus/summaries/2011 June Memo.json"
+    with open(json_path, "r") as jsonfile:
+        data: dict = json.load(jsonfile)
+    unique_words, frequencies = list(data.keys()), list(data.values())
+
+    index = np.arange(len(unique_words[:limit]))
     plt.figure(figsize=(13, 4), dpi=400)
-    plt.bar(index, frequencies[:70])
+    plt.bar(index, frequencies[:limit])
     plt.xlabel('Word', fontsize=5)
     plt.ylabel('Frequency', fontsize=5)
-    plt.xticks(index, unique_words[:70], fontsize=5, rotation=60)
+    plt.xticks(index, unique_words[:limit], fontsize=5, rotation=60)
     plt.title('Frequency of words in CSC Papers')
-
-    plt.savefig("Figure1.png")
-    plt.show()
+    plt.tight_layout()
+    plt.savefig(json_path.split(".")[0] + ".png")
+    # plt.show()
 
 
 def main():
     # Process the corpus of memos:
-    corpus_dictionary = process_pdfs("corpus")
-
-    # words, frequencies = get_words_and_freqs(process_pdfs("corpus", reuse=True))
+    pdfs_to_texts()
+    json_to_graph("corpus/summaries/2011 June Memo.json")
 
     # Process the corpusses of each topic:
-    topic_pdfs = glob.glob("topics/pdfs/*.pdf")
-    for topic_pdf in topic_pdfs:
-        pass
+    # topic_pdfs = glob.glob("topics/pdfs/*.pdf")
+    # for topic_pdf in topic_pdfs:
+    #     pass
 
     # words, frequencies = get_words_and_freqs(process_pdfs("_topics", reuse=True))
     # plot_bar_graph(words, frequencies)
--- reading_list.md
+++
@@ -1,6 +0,0 @@
-[Visualizing Word Vectors with t-SNE](https://www.kaggle.com/jeffd23/visualizing-word-vectors-with-t-sne)
-
-
-[How We Visualized Semantics and Similarity](https://blog.interactivethings.com/how-we-visualized-semantics-and-similarity-2a5203479011)
-
-
