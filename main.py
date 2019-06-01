b6d5bc0a6601c9d8256fc20cad05b5fcd7f2f74b
beyarkay <boydrkane@gmail.com>
2019-06-01 15:37:56 +0200

    Made code work better with separate projects

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import glob
 import json
+import math
 import os
 
 import numpy as np
@@ -11,19 +12,19 @@ from wand.color import Color
 from wand.image import Image as WandImage
 
 
-def get_filename(pdf_path: str) -> str:
+def get_filename(file_path: str) -> str:
     """
     Utility method to just get the filename from a path, excluding the extension and parent directories
-    :param pdf_path:
+    :param file_path:
     :return:
     """
-    return pdf_path.split(os.sep)[-1].split(".")[0]
+    return file_path.split(os.sep)[-1].split(".")[0]
 
 
-def get_text_from_pdf(pdf_path: str, root: str, verbose: bool = True, reuse=True) -> list:
+def get_text_from_pdf(root: str, pdf_path: str, verbose: bool = True, reuse=True) -> list:
     """
     Use Tesseract OCR to get the text from filename. PDFs are converted pagewise into
-    jpegs and saved in root/pngs/[file_name]/
+    jpegs and saved in savepath
     Further Tesseract OCR usage instructions: https://anaconda.org/jiayi_anaconda/pytesseract
     :param root:
     :param reuse:
@@ -45,7 +46,7 @@ def get_text_from_pdf(pdf_path: str, root: str, verbose: bool = True, reuse=True
         image.alpha_channel = 'remove'
 
         # Each pdf has to be split into multiple .pngs, so each pdf gets a directory in root/pngs/
-        images_directory = os.path.join(root, "pngs", get_filename(pdf_path))
+        images_directory = os.path.join(os.path.join(root, "pngs"), get_filename(pdf_path))
 
         os.makedirs(images_directory, exist_ok=True)
         if not reuse or len(os.listdir(images_directory)) == 0:
@@ -76,12 +77,11 @@ def pdfs_to_texts(root: str, verbose: bool = True, reuse: bool = True) -> None:
     :param reuse: If False, recreate the image and txt file, even if those files already exist
     :return: None
     """
-    directory_with_pdfs = os.path.join(root, "pdfs")
-    pdf_paths = sorted(glob.glob(os.path.join(directory_with_pdfs, "*.pdf")))
+    pdf_paths = sorted(glob.glob(os.path.join(os.path.join(root, "pdfs"), "*.pdf")))
 
     for i, pdf_path in enumerate(pdf_paths, 1):
         file_id = get_filename(pdf_path)
-        text_file_path = os.path.join(root, "txts", file_id + ".txt")
+        text_file_path = os.path.join(os.path.join(root, "txts"), file_id + ".txt")
 
         if not reuse or not os.path.exists(text_file_path):
             # The user doesn't want to reuse the already-processed files, or those files don't exist
@@ -90,23 +90,24 @@ def pdfs_to_texts(root: str, verbose: bool = True, reuse: bool = True) -> None:
 
             # Write the data to the appropriate text file
             with open(text_file_path, "w+") as text_file:
-                text_file.writelines(get_text_from_pdf(pdf_path, root=root, verbose=verbose, reuse=reuse))
+                text = get_text_from_pdf(pdf_path, os.path.join(root, "pngs"), verbose=verbose, reuse=reuse)
+                text_file.writelines(text)
         else:
             # already-processed files exist, and the user wants to use them
             if verbose:
                 print("'{}' Already exists. ({} out of {})".format(pdf_path, i, len(pdf_paths)))
 
 
-def texts_to_jsons(root: str, stopwords_file: str = "stopwords.txt", verbose: bool = True) -> None:
+def texts_to_jsons(root: str, stopwords_path: str, verbose: bool = True) -> None:
     """
     Look in root/txts/ and convert the raw text into json files with (word : frequency) pairs, saved to root/summaries
-    :param root: Root file in which to work. Should contain root/txts and root/summaries
-    :param stopwords_file: a path to a textfile, newline-deliminated list of words to exclude from the json file.
+    :param root:
+    :param stopwords_path: a path to a textfile, newline-deliminated list of words to exclude from the json file.
     :param verbose: if True, log progress to the console
     :rtype: None
     """
-    txt_files = sorted(glob.glob(os.path.join(root, "txts", "*.txt")))
-    with open(stopwords_file, "r") as stopwords_txt:
+    txt_files = sorted(glob.glob(os.path.join(os.path.join(root, "txt"), "*.txt")))
+    with open(stopwords_path, "r") as stopwords_txt:
         stopwords = set([word.strip() for word in stopwords_txt.readlines()])
 
     for i, txt_file in enumerate(txt_files, 1):
@@ -130,7 +131,7 @@ def texts_to_jsons(root: str, stopwords_file: str = "stopwords.txt", verbose: bo
         frequencies = [item[1] for item in sorted_words]
 
         # write as json file
-        json_path = os.path.join(root, "summaries", get_filename(txt_file) + ".json")
+        json_path = os.path.join(os.path.join(root, "summaries", "jsons"), get_filename(txt_file) + ".json")
         json_dict = {word: freq for word, freq in zip(clean_words, frequencies)}
         with open(json_path, "w+") as json_file:
             json.dump(json_dict, json_file, indent=2)
@@ -173,7 +174,7 @@ def json_to_graph(json_path: str, num_words: int = 50, verbose: bool = True) -> 
     # plt.show()
 
 
-def graph_memo_composition(memo_json_path: str, verbose: bool = True) -> None:
+def graph_memo_composition(root: str, memo_json_path: str, verbose: bool = True) -> None:
     """
     Graph and save a single pie chart, that shows the composition of the pdf represented by memo_json_path.
     The different categories represented by the wedges of the pie are the different json files found in topics/summaries
@@ -189,7 +190,7 @@ def graph_memo_composition(memo_json_path: str, verbose: bool = True) -> None:
     memo_words, memo_freqs = list(data.keys()), list(data.values())
     topic_scores = {}
 
-    topic_paths = glob.glob("topics/summaries/*.json")
+    topic_paths = glob.glob(os.path.join(root, "summaries", "jsons", "*.json"))
     if len(topic_paths) == 0:
         print("No topic files found that match with '{}'. Graphing failed".format("topics/summaries/*.json"))
         return
@@ -225,11 +226,55 @@ def graph_memo_composition(memo_json_path: str, verbose: bool = True) -> None:
     # current_palette = sns.color_palette()
     ax.set_title("Composition of " + get_filename(memo_json_path).title())
 
-    pie_directory = os.path.join(os.sep.join(memo_json_path.split(os.sep)[:-1]), "pies")
+    pie_directory = os.path.join(root, "summaries", "pies")
     os.makedirs(pie_directory, exist_ok=True)
     pie_path = os.path.join(pie_directory, get_filename(memo_json_path) + "-pie_chart.png")
     plt.tight_layout()
     plt.savefig(pie_path)
+
+
+def create_test_topic(root, topic_id, total_unique_words=100):
+    words = [topic_id + f"{i:0{len(str(total_unique_words))}}" for i in range(total_unique_words)]
+
+    def freq_distribution(x):
+        return math.ceil((0.4 * total_unique_words) / x)
+
+    freqs = [math.ceil(freq_distribution(x + 1)) for x in range(total_unique_words)]
+
+    json_dict = {word: freq for word, freq in zip(words, freqs)}
+    json_path = os.path.join(root, "summaries", "jsons", "*.json")
+    with open(json_path, "w+") as json_file:
+        json.dump(json_dict, json_file, indent=2)
+
+
+def create_test_memo(root="test_files/topics"):
+    return
+    # test_length = 20
+    # word_freqs = {}
+    # topics = glob.glob(f"{root}/summaries/*.json")
+    # topic_wfs = {}
+    # for topic in topics:
+    #     with open(topic, "r") as json_file:
+    #         topic_wfs.update(json.load(json_file))
+    # for word in range(test_length):
+    #
+    #
+    #
+    # pass
+
+
+def create_directory_structure(root: str):
+    level1 = ["corpus", "topics"]
+    level2 = ["pdfs", "pngs", "summaries", "txts"]
+
+    for l1 in level1:
+        for l2 in level2:
+            if l2 is "summaries":
+                os.makedirs(os.path.join(root, l1, l2, "bars"), exist_ok=True)
+                os.makedirs(os.path.join(root, l1, l2, "pies"), exist_ok=True)
+                os.makedirs(os.path.join(root, l1, l2, "jsons"), exist_ok=True)
+            else:
+                os.makedirs(os.path.join(root, l1, l2), exist_ok=True)
 
 
 def developement_main() -> None:
@@ -242,10 +287,13 @@ def developement_main() -> None:
     sns.set()
     # pdfs_to_texts("corpus")
     # texts_to_jsons("corpus")
-    memo_json_paths = glob.glob("corpus/summaries/*.json")
-    for memo_json_path in memo_json_paths:
-        json_to_graph(memo_json_path)
-        graph_memo_composition(memo_json_path)
+    # memo_json_paths = glob.glob("corpus/summaries/*.json")
+    # for memo_json_path in memo_json_paths:
+    #     json_to_graph(memo_json_path)
+    #     graph_memo_composition(memo_json_path)
+
+    for i in range(5):
+        create_test_topic("test_files", chr(i + ord("a")), total_unique_words=20)
 
 
 def main() -> None:
